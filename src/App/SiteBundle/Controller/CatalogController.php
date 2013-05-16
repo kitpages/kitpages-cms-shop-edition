e93624ad78a019c1ec70e825424cc5239e7eaa1b
Sebastien Lefebvre <sebastien.lefebvre@kitpages.fr>
2013-05-16 17:59:44 +0200

    update category

--- src/App/SiteBundle/Controller/CatalogController.php
+++ src/App/SiteBundle/Controller/CatalogController.php
@@ -4,10 +4,16 @@
 namespace App\SiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Kitpages\CmsBundle\Model\Paginator;
-use App\SiteBundle\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Kitpages\CmsBundle\Controller\Context;
+use Kitpages\CmsBundle\Entity\NavPublish;
+use Kitpages\CmsBundle\Entity\Page;
+use Kitpages\CmsBundle\Entity\PagePublish;
+use Kitpages\CmsBundle\Controller\NavController;
 
-class CatalogController extends Controller
+class CatalogController extends NavController
 {
 
     public function categoryAction($categoryName)
@@ -16,5 +22,109 @@ class CatalogController extends Controller
     }
 
 
+    public function widgetProductListAction($slug) {
+        $em = $this->getDoctrine()->getManager();
+        $context = $this->get('kitpages.cms.controller.context');
+        $pageList = array();
+        if ($context->getViewMode() == Context::VIEW_MODE_EDIT || $context->getViewMode() == Context::VIEW_MODE_PREVIEW) {
+            $page = $em->getRepository('KitpagesCmsBundle:Page')->findOneBySlug($slug);
+            if ($page != null) {
+                $pageChildren = $em->getRepository('KitpagesCmsBundle:Page')->childrenOfDepth($page, 1);
+                $cmsFileManager = $cmsFileManager = $this->get('kitpages.cms.manager.file');
+                $dataInheritanceList = $this->container->getParameter('kitpages_cms.page.data_inheritance_list');
+                foreach($pageChildren as $page) {
+                    $dataRoot = $em->getRepository('KitpagesCmsBundle:Page')->getDataWithInheritance($page, $dataInheritanceList);
+                    $description = '';
+                    if (isset($dataRoot['description'])){
+                        $description = $dataRoot['description'];
+                    }
+                    $pageList[] = array(
+                        'menuTitle' => $page->getMenuTitle(),
+                        'description' => $description,
+                        'url' => $this->getPageLink($page),
+                        'mediaList' => $cmsFileManager->mediaList($dataRoot, false)
+                    );
+                }
+            }
+
+        }
+
+        elseif ($context->getViewMode() == Context::VIEW_MODE_PROD) {
+            // calculate page
+            $cacheManager = $this->get('kitpages.simple_cache');
+
+            $myThis = $this;
+            $response = $cacheManager->get(
+                'app-site-category-'.$context->getViewMode()."-$slug",
+                function() use ($myThis, $em, $slug ) {
+                    $em = $myThis->getDoctrine()->getManager();
+                    $context = $myThis->get('kitpages.cms.controller.context');
+
+                    $page = $em->getRepository('KitpagesCmsBundle:Page')->findOneBySlug($slug);
+
+                    if ($page != null) {
+                        $pageChildren = $em->getRepository('KitpagesCmsBundle:Page')->childrenOfDepth($page, 1);
+                        $cmsFileManager = $cmsFileManager = $this->get('kitpages.cms.manager.file');
+                        $dataInheritanceList = $this->container->getParameter('kitpages_cms.page.data_inheritance_list');
+                        foreach($pageChildren as $page) {
+                            $dataRoot = $em->getRepository('KitpagesCmsBundle:Page')->getDataWithInheritance($page, $dataInheritanceList);
+                            $pageList[] = array(
+                                'menuTitle' => $page->getMenuTitle(),
+                                'url' => $this->getPageLink($page),
+                                'mediaList' => $cmsFileManager->mediaList($dataRoot, false)
+                            );
+                        }
+                    }
+                    return $myThis->render(
+                        'AppSiteBundle:Catalog:widget-product-list.html.twig',
+                        array(
+                            'pageList' => $pageList,
+                            'kitCmsViewMode' => $context->getViewMode(),
+                            'kitpages_target' => $_SERVER["REQUEST_URI"]
+                        )
+                    );
+                }
+            );
+            return $response;
+        }
+        return $this->render(
+            'AppSiteBundle:Catalog:widget-product-list.html.twig',
+            array(
+                'pageList' => $pageList,
+                'kitCmsViewMode' => $context->getViewMode(),
+                'kitpages_target' => $_SERVER["REQUEST_URI"]
+            )
+        );
+    }
+
+    public function getPageLink($page) {
+        $url = '';
+        if ($page->getPageType() == 'link' ) {
+            $url = $page->getLinkUrl();
+            if ($page->getIsLinkUrlFirstChild()) {
+                $em = $this->getDoctrine()->getManager();
+                $pageChildren = $em->getRepository('KitpagesCmsBundle:Page')->children($page, true);
+                if (count($pageChildren) > 0 && $pageChildren['0'] instanceof Page) {
+                    $url = $this->getPageLink($pageChildren['0']);
+                }
+            }
+        }
+        if ($page->getPageType() == 'edito' ) {
+            if ($page->getForcedUrl()) {
+                $url = $this->getRequest()->getBaseUrl().$page->getForcedUrl();
+            } else {
+                $url = $this->generateUrl(
+                    'kitpages_cms_page_view_lang',
+                    array(
+                        'id' => $page->getId(),
+                        'lang' => $page->getLanguage(),
+                        'urlTitle' => $page->getUrlTitle()
+                    )
+                );
+            }
+        }
+        return $url;
+
+    }
 
 }
---
+++ src/App/SiteBundle/Form/Page/PageLayoutEditCategory.php
@@ -0,0 +1,51 @@
+<?php
+namespace App\SiteBundle\Form\Page;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class PageLayoutEditCategory extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('media_mainImage', 'hidden');
+        $builder->add(
+            'metaTitle',
+            'text',
+            array(
+                'label' => 'Meta Title',
+                'required' => false,
+                'attr' => array(
+                    "size" => '50'
+                )
+            )
+        );
+        $builder->add(
+            'metaDescription',
+            'text',
+            array(
+                'label' => 'Meta Description',
+                'required' => false,
+                'attr' => array(
+                    "size" => '100'
+                )
+            )
+        );
+        $builder->add(
+            'metaKeywords',
+            'text',
+            array(
+                'label' => 'Meta Keywords',
+                'required' => false,
+                'attr' => array(
+                    "size" => '100'
+                )
+            )
+        );
+    }
+
+    public function getName() {
+        return 'PageLayoutEditDefault';
+    }
+
+}
